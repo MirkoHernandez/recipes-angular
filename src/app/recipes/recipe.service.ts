834e6106ea0f09c18b885ed3405f80c468485fc1
h <h@h.d>
2019-05-26 17:29:52 -0400

    Include ingredients in the recipe model. Modify the recipes component to display ingredients and add them to the shopping list.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,43 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Recipe1', 'Description1', 'Image1'),
-        new Recipe('Recipe20', 'Description20', 'Image20'),
+        new Recipe('Recipe1',
+            'Description1',
+            'Image1',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 6)
+            ]),
+        new Recipe('Recipe20',
+            'Description20',
+            'Image20',
+            [
+                new Ingredient('Buns', 3),
+                new Ingredient('Meat', 2)
+            ]),
     ];
+
+    constructor(private shoppingListService: ShoppingListService) {
+
+    }
+
+
 
     getRecipes() {
         return this.recipes.slice();
     }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
+    }
+
+
+
 }
